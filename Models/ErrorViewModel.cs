94f4d83a96e5e55d78ca2864c494d66d0d72f7f7
EMILIA INÉS ARMENO <47697893@ort>
2023-09-27 11:28:21 -0300

    creamo el proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tp1_javascript_login.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
