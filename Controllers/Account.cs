c95aa96245bff2b7507ba84109010dd30d4f476d
EMILIA INÉS ARMENO <47697893@ort>
2023-09-28 15:34:48 -0300

    mucho mucho avance

--- Controllers/Account.cs
+++ Controllers/Account.cs
@@ -15,7 +15,71 @@ public class Account : Controller
 
     public IActionResult Registro ()
     {
+
         return View();
     }
 
+
+
+    public IActionResult TraerDatosDelUsuario(){
+        List<Entidad> _listaUsuarios=new List <Entidad>();
+        _listaUsuarios=BD.ObtenerUsuarios();
+        return View("Bienvenida");
+    }
+
+    // EN REGISTRO verifica si el usuario ya existe y verifica si la contraseña cumple con todos los requerimientos
+    [HttpPost]
+    public IActionResult VerificarRegistro( string _username, string _contra, string _repe, string _nombre, string _gmail, int _tel)
+    {
+        List<Entidad> _listaUsuarios=new List<Entidad>();
+        _listaUsuarios=BD.ObtenerUsuarios();
+        bool ok = false;
+        
+        foreach (Entidad unUsr in _listaUsuarios)
+        {
+            if (unUsr.Username != _username)
+            {
+                if (_contra==_repe)
+                {
+                    ok = true;
+                    return View("RegistroExitosos");
+                }
+            } 
+            else
+            {
+                return View ("RegistroNoExitoso");
+            }    
+        }
+        return ok;
+    }
+
+
+    public IActionResult VerificarIngresos (string _username, string _contra) // EN LOGIN verifica que el usuario exista y que la contraseña sea correcta
+    {
+        List<Entidad> _listaUsuarios=new List<Entidad>();
+        _listaUsuarios=BD.ObtenerUsuarios();
+        bool ok;
+        foreach (Entidad unUsr in _listaUsuarios)
+        {
+            if (unUsr.Username == _username)
+            {
+                if (unUsr.Contrasenia==_contra)
+                {
+                    ok = true;
+                    return View("Bienvenida");
+                }    
+            }      
+        }
+    }
 }
+
+
+
+
+
+
+//LOGIN te pide usuario y contraseña. tenes que verificar si la cuenta está registrada. cuando entras te salta un cartel de bienvenida y si te equivocaste de contraseña te manda a olvidar a contraseña
+//REGISTRO te pide usuario, nombre, contraseña 2 veces, mail y telefono. la contraseña debe de tener por lo menos, un carácter especial, una letra en mayúscula y al menos 8 caracteres. Cuando entras te manda un cartel de cuenta creada y un lin a iniciar sesión. EN LA MISMA PÄGINA, si la cuenta ya fue creada anteriormente te dice que ya existe esa cuenta y te pone un link de iniciar sesión
+//OLVIDE te pregunta tu usuario y te da tu contraseña
+//BIENVENIDA te muestra los datos de tu cuenta
+//EN BD traer lista con todos los usuarios
--- Models/BD.cs
+++ Models/BD.cs
@@ -2,4 +2,16 @@ using System.Data.SqlClient;
 using Dapper;
 public class BD{
     private static string _connectionString = @"Server=localhost; DataBase=LOGIN;Trusted_Connection=True;";
+
+
+
+    public static List<Entidad> ObtenerUsuarios(){ //traemos lista de usuarios
+        string sql = "SELECT * FROM USUARIO";
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+            return db.Query<Entidad>(sql).ToList();
+        }
+    }
+
+
+
 }
