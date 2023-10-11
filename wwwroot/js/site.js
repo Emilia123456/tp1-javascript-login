8fbad6e6e635c99e7bcb47474e88b87fd0f34295
EMILIA INÉS ARMENO <47697893@ort>
2023-10-11 11:43:43 -0300

    falta encriptación

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//necesitamos traer la contraseña
+const contra = document.getElementById("contrasenia");
+const boton =document.getElementById("boton");
+boton.disabled=true
 
-// Write your JavaScript code.
+const especiales = [
+    "!","#","$","%","&","/","@"
+]
+
+const mayúsculas = [
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+]
+
+contra.addEventListener("input", () => {
+    let valor = contra.value 
+    const tieneEspeciales = especiales.some(Caracter => valor.includes(Caracter))
+    const tieneMayúsculas = mayúsculas.some(Caracter => valor.includes(Caracter))
+    if(tieneEspeciales && tieneMayúsculas && valor.length >= 8){
+        contra.style.border="2px solid green"
+        boton.enabled = true
+    }else{
+        contra.style.border="2px solid red"
+        
+    }
+})
